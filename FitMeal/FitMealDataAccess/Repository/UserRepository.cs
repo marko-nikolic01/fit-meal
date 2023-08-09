50019c100258fbde4e1cd66e16897fdcb4bab88b
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-08-09 15:06:45 +0200

    Added an implementation of IUserRepository.

---
+++ FitMeal/FitMealDataAccess/Repository/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using FitMealDataAccess.Context;
+using FitMealDataAccess.Repository.IRepository;
+using FitMealModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitMealDataAccess.Repository
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(User user)
+        {
+            _db.Users.Update(user);
+        }
+    }
+}
