c2efd81d32efeb0b2565ad3481f2078e576de585
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-08-09 15:09:39 +0200

    Added implementation of IUnitOfWork.

---
+++ FitMeal/FitMealDataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using FitMealDataAccess.Context;
+using FitMealDataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitMealDataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public IUserRepository UserRepository { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            UserRepository = new UserRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
