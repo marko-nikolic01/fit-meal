3e9b01fa29dee2aaad683afb95004f1ae90e556f
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-08-09 15:08:03 +0200

    Added IUnitOfWork.

---
+++ FitMeal/FitMealDataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitMealDataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+
+        void Save();
+    }
+}
+}
