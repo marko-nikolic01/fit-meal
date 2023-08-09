7c7a3454602b78824033e97c9b6c69f589fe9f64
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-08-09 15:05:20 +0200

    Added IUserRepository.

---
+++ FitMeal/FitMealDataAccess/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using FitMealModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitMealDataAccess.Repository.IRepository
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        void Update(User user);
+    }
+}
