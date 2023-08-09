29ad81e0b4a4d7b15ee801b9bd0874d79e5bbd11
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-08-09 15:04:07 +0200

    Added a generic implementation of IRepository.

---
+++ FitMeal/FitMealDataAccess/Repository/Repository.cs
@@ -0,0 +1,66 @@
+﻿using FitMealDataAccess.Context;
+using FitMealDataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitMealDataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll(string? includedProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (!string.IsNullOrEmpty(includedProperties))
+            {
+                foreach (string property in includedProperties.Split(','))
+                {
+                    query = query.Include(property);
+                }
+            }
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includedProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            if (!string.IsNullOrEmpty(includedProperties))
+            {
+                foreach (string property in includedProperties.Split(','))
+                {
+                    query = query.Include(property);
+                }
+            }
+            return query.FirstOrDefault();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
