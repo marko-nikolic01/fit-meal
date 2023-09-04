c8ae63047faebee2b905591d4d495d21f21cb7d0
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-09-04 16:13:43 +0200

    Added Classes for Food and Nutrients.

---
+++ FitMeal/FitMealModels/Nutrient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitMealModels
+{
+    public class Nutrient
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Name is required.")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Unit is required.")]
+        [MaxLength(10)]
+        public string Unit { get; set; }
+
+
+        [Required(ErrorMessage = "Ammount is required.")]
+        [Range(0, 10000)]
+        public double Ammount { get; set; }
+    }
+}
