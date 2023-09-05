2bf894ebcaf1f6ed3499c07016533247f56939e6
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-09-05 17:45:54 +0200

    Added an interface for FoodAPIResponse.

---
+++ FitMeal/FitMealUtilities/Converters/IConverter/IFoodAPIResponseConverter.cs
@@ -0,0 +1,14 @@
+﻿using FitMealModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitMealUtilities.Converters.IConverter
+{
+    public interface IFoodAPIResponseConverter
+    {
+        List<Food> ConvertFoodSearch(string response);
+    }
+}
