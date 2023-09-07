d35c5c0d4fe1fbe8cc3c0530aa29949eb2a06d8a
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-09-07 20:22:07 +0200

    Added colors for selected tabs.

--- FitMeal/reactapp/src/components/NavigationBar.jsx
+++ FitMeal/reactapp/src/components/NavigationBar.jsx
@@ -1,19 +1,21 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './styles/NavigationBar.css'
 import NavigationBarItem from './NavigationBarItem.jsx'
 
 function NavigationBar(props) {
     const { theme } = props
+    const [selectedTab, setSelectedTab] = useState('Home')
 
     const navigate = useNavigate();
 
     return (
         <>
-            <div className={`navigation-bar background-${theme}-primary`}>
-                <NavigationBarItem theme={theme} content='Home' />
-                <NavigationBarItem theme={theme} content='Foods' />
-                <NavigationBarItem theme={theme} content='Diary' />
-                <NavigationBarItem theme={theme} content='Meal maker' />
+            <div className={`navigation-bar`}>
+                <NavigationBarItem theme={theme} content='Home' isSelected={selectedTab === 'Home' ? true : false} setSelectedTab={setSelectedTab} />
+                <NavigationBarItem theme={theme} content='Foods' isSelected={selectedTab === 'Foods' ? true : false} setSelectedTab={setSelectedTab} />
+                <NavigationBarItem theme={theme} content='Diary' isSelected={selectedTab === 'Diary' ? true : false} setSelectedTab={setSelectedTab} />
+                <NavigationBarItem theme={theme} content='Meal maker' isSelected={selectedTab === 'Meal maker' ? true : false} setSelectedTab={setSelectedTab} />
             </div>
         </>
     )
--- FitMeal/reactapp/src/components/NavigationBarItem.jsx
+++ FitMeal/reactapp/src/components/NavigationBarItem.jsx
@@ -1,11 +1,16 @@
 import './styles/NavigationBarItem.css'
 
 function NavigationBarItem(props) {
-    const { theme, content } = props
+    const { theme, content, isSelected, setSelectedTab } = props
+
+    function navigate() {
+        console.log(content)
+        setSelectedTab(content)
+    }
 
     return (
         <>
-            <div className={`navigation-bar-item background-${theme}-primary text-${theme}-primary`}>
+            <div className={`navigation-bar-item navigation-bar-item-background-${theme}-${isSelected ? 'secondary' : 'primary'} text-${theme}-${isSelected ? 'secondary' : 'primary'}`} onClick={navigate}>
                 <div className={`navigation-bar-item-content`}>
                     {content}
                 </div>
