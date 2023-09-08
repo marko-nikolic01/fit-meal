6591950a6e9c62ae2e9242d1dd3b18f41bbbafcc
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-09-08 16:56:32 +0200

    Styled the search bar.

--- FitMeal/reactapp/src/components/Foods.jsx
+++ FitMeal/reactapp/src/components/Foods.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react"
 import './styles/Foods.css'
 import UnauthorizedAccess from './UnauthorizedAccess.jsx'
 
 function Foods(props) {
     const { theme, isUserAuthenticated } = props
+    const [search, setSearch] = useState("opened")
+
+    function handleChange(event) {
+        const { value } = event.target
+        setSearch(value)
+    }
 
     return (
         <>
             {isUserAuthenticated
                 ? <div className={`foods background-${theme}-secondary`}>
                     <div className='foods-search-bar'>
-                        <input className={`foods-search-bar-input text-${theme}-primary background-${theme}-primary border-${theme}-primary`} type="text" placeholder="Search"/>
-                        <button className='foods-search-bar-button'>
-                            <img className="foods-search-bar-button-icon"/>
+                        <input className={`foods-search-bar-input text-${theme}-primary background-${theme}-primary border-${theme}-secondary`} type="text" placeholder="Search"/>
+                        <button className={`foods-search-bar-button button-${theme}-primary`}>
+                            <img className='foods-search-bar-button-icon' src="./images/Search.svg"></img>
                         </button>
                     </div>
                 </div>
