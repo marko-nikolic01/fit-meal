27ce1371b6c03318fc2ef0f7f0ebd788265464d9
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-09-08 17:14:01 +0200

    Added FoodAPIController.

---
+++ FitMeal/webapi/Controllers/FoodAPIController.cs
@@ -0,0 +1,42 @@
+﻿using FitMealModels;
+using FitMealModels.DTO;
+using FitMealServices.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitMealAPI.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class FoodPIController : ControllerBase
+    {
+        private readonly IFoodAPIService _foodAPIService;
+        public FoodPIController(IFoodAPIService foodAPIService)
+        {
+            this._foodAPIService = foodAPIService;
+        }
+
+
+        [HttpPost("signup")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [Produces("application/json")]
+        public async Task<ActionResult> SignUp([FromBody] SignUpDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (_signUpService.IsUsernameOrEmailTaken(dto.Username, dto.Email))
+            {
+                return Conflict(new { Message = "Username or email is already in use." });
+            }
+
+            User user = new User(dto.Email, dto.Username, dto.Password);
+            _signUpService.SignUp(user);
+            string token = _JWTService.Generate(user);
+            return Ok(new { Token = token });
+        }
+    }
+}
