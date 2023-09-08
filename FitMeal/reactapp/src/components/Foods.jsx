5a33fae9a10fdcb3bd243894067cbd52cc487935
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-09-08 15:33:00 +0200

    Added Foods component.

---
+++ FitMeal/reactapp/src/components/Foods.jsx
@@ -0,0 +1,24 @@
+import './styles/Foods.css'
+import UnauthorizedAccess from './UnauthorizedAccess.jsx'
+
+function Foods(props) {
+    const { theme, isUserAuthenticated } = props
+
+    return (
+        <>
+            {isUserAuthenticated
+                ? <div className='foods'>
+                    <div className='foods-search-bar'>
+                        <input className={`foods-search-bar-input text-${theme}-primary background-${theme}-primary border-${theme}-primary`} type="text" placeholder="Search"/>
+                        <button className='foods-search-bar-button'>
+                            <img className="foods-search-bar-button-icon"/>
+                        </button>
+                    </div>
+                </div>
+                : <UnauthorizedAccess theme={theme} />
+            }
+        </>
+    )
+}
+
+export default Foods
